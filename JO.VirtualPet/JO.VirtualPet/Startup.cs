e802b70452271e3062f06a1492c544a2d90d72b8
James O'Neill <gingeoneill@gmail.com>
2017-07-21 21:18:52 +0100

    Changed to use AddDbContext

--- JO.VirtualPet/JO.VirtualPet/Startup.cs
+++ JO.VirtualPet/JO.VirtualPet/Startup.cs
@@ -19,12 +19,12 @@ namespace JO.VirtualPet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VirtualPetContext>();
             // Add framework services.
             services.AddMvc();
 
             services.AddLogging();
             // Add our repository type
-            services.AddTransient<VirtualPetContext>();
             services.AddTransient<IRepository<Animal>,EfRepository<Animal>>();
             services.AddTransient<IRepository<AnimalStats>, EfRepository<AnimalStats>>();
             services.AddTransient<IRepository<AnimalType>, EfRepository<AnimalType>>();
